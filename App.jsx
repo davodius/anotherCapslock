9271183bbd90ff84de4c45392f66233b9d84c429
davodius <davidhanebrant@gmail.com>
2024-06-19 20:31:16 +0200

    first commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TextTransformer from "./TextTransformer";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Text Transformer</h1>
+      <TextTransformer />
+    </div>
+  );
+};
+
+export default App;
